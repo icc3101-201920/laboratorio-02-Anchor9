17e91885ee65bc0b0ad358d8b6b0b81df7f5a649
unknown <fjmiranda4iuandes.cl>
2019-08-16 09:22:55 -0400

    L2

---
+++ Laboratorio_1_OOP_201902/Laboratorio_1_OOP_201902/Card/Card.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_1_OOP_201902.Card
+{
+    class Card
+    {
+        //Esta es mi clase padre y ahora tengo que ver que tienen en comun las clases Special y Combat
+
+        private string name;
+        private string type;
+        private string effect;
+
+
+        public Card(string name, string type, string effect, int attackPoints, bool hero)
+        {
+            Name = name;
+            Type = type;
+            Effect = effect;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return this.type;
+            }
+            set
+            {
+                this.type = value;
+            }
+        }
+        public string Effect
+        {
+            get
+            {
+                return this.effect;
+            }
+            set
+            {
+                this.effect = value;
+            }
+        }
+
+    }
+}
--- Laboratorio_1_OOP_201902/Laboratorio_1_OOP_201902/Card/SpecialCard.cs
+++ Laboratorio_1_OOP_201902/Laboratorio_1_OOP_201902/Card/SpecialCard.cs
@@ -4,45 +4,19 @@ using System.Text;
 
 namespace Laboratorio_1_OOP_201902.Card
 {
-    public class SpecialCard
+    class SpecialCard : Card
     {
-        //Atributos
-        private string name;
-        private string type;
+
         private string buffType;
-        private string effect;
+        
 
         //Constructor
-        public SpecialCard(string name, string type, string effect)
+        public SpecialCard(Nullable BuffType)
         {
-            Name = name;
-            Type = type;
-            Effect = effect;
-            BuffType = null;
+           BuffType = null;
         }
 
-        //Propiedades
-        public string Name
-        {
-            get
-            {
-                return this.name;
-            }
-            set
-            {
-                this.name = value;
-            }
-        }
-        public string Type
-        {
-            get
-            {
-                return this.type;
-            }
-            set
-            {
-                this.type = value;
-            }
+
         }
         //Propiedades
         public string BuffType
@@ -55,19 +29,6 @@ namespace Laboratorio_1_OOP_201902.Card
             {
                 this.buffType = value;
             }
-        }
-        public string Effect
-        {
-            get
-            {
-                return this.effect;
-            }
-            set
-            {
-                this.effect = value;
-            }
-        }
-
 
     }
 }
